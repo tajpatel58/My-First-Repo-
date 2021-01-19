a7486840f3c18ce0c7bf8eb62589fd3a32e3e90f
Taj Patel <tajpatel58@gmail.com>
2021-01-19 19:56:09 +0000

    1st commit

---
+++ practice.py
@@ -0,0 +1,11 @@
+
+# A little practice of uploading to remote repo.
+
+
+def sum_of_n_cubed(n):
+    total = 0
+    for j in range(1, n+1):
+        total += j**3
+    return total
+
+print(sum_of_n_cubed(3))
